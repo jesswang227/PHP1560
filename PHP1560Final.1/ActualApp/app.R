a2f6dcd3d83565cf6de7008f1f135f7c03f2edd2
jesswang227 <jessica_wang3@brown.edu>
2021-12-14 01:41:41 -0500

    Update app.R

--- PHP1560Final.1/ActualApp/app.R
+++ PHP1560Final.1/ActualApp/app.R
@@ -59,18 +59,23 @@ library(shiny)
 
 
 ui <- fluidPage(
-    titlePanel("censusVis"),
+    titlePanel("Austin Real Estate Price Estimator 2017-2021"),
 
     sidebarLayout(
       sidebarPanel(
-        helpText("Create demographic maps with
-                 information from the 2010 US Census."),
+        helpText("Find an estimated price of a property using its characteristics below."),
 
-        numericInput("num", "Number one", value = 0, min = 0, max = 100)
+        numericInput("propTRInput", "Property Tax Rate", value = 0, min = 0, max = 3),
+        numericInput("garageSpacesInput", "Number of Garage Spaces", value = 0, min = 0, max = 100),
+        numericInput("hasCoolingInput", "If property has air conditioning or not (1 for yes, 0 for no", value = 0, min = 0, max = 1),
+        numericInput("hasGarageInput", "If property has garage or not (1 for yes, 0 for no", value = 0, min = 0, max = 1),
+        numericInput("hasAssociationInput", "If property has homeowner's association or not (1 for yes, 0 for no", value = 0, min = 0, max = 1),
+        numericInput("hasHeatingInput", "If property has heating or not (1 for yes, 0 for no", value = 0, min = 0, max = 1),
+        numericInput("hasSpaInput", "If property has spa or not (1 for yes, 0 for no", value = 0, min = 0, max = 1),
       ),
 
       mainPanel(
-        textOutput("selected_var"),
+        textOutput("propTROutput"),
         textOutput("min_max")
       )
     )
@@ -78,8 +83,8 @@ ui <- fluidPage(
 
   server <- function(input, output) {
 
-    output$selected_var <- renderText({
-      paste("You have selected", input$num)
+    output$propTROutput <- renderText({
+      paste("You have selected", input$propTRInput)
     })
 
 
